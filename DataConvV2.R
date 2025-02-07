dff4049380b614a0bce8b01371977120ed9bc18d
Zach Ingram <ingramzach@gmail.com>
2025-02-07 03:41:40 -0600

    added title to plots

--- DataConvV2.R
+++ DataConvV2.R
@@ -5,10 +5,11 @@ output_folder = "<path>"
 file_list = list.files(path=input_folder, pattern="\\.spc$", full.names=TRUE)
 for (file in file_list) {
   raw_contents = read.spc(file, log.txt=FALSE)
-  plot(raw_contents)
   cleaned_data = as.data.frame(raw_contents$spc)
   transposed_data = t(cleaned_data)
   file_name = tools::file_path_sans_ext(basename(file))
   output_file = file.path(output_folder, paste0(file_name, ".csv"))
   write.csv(transposed_data, file=output_file)
+  plotspc(raw_contents)
+  title(file_name)
 }
